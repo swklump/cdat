05e17a373a946409653553e8049237059db1c688
Klump, Samuel W <samuel.klump@hdrinc.com>
2019-04-22 18:53:26 -0500

    Upload New File

---
+++ CAT/CAT/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('', views.analysis, name='CDAT-home'),
+    path('analysis/', views.analysis, name='CDAT-analysis'),
+    path('analysis/file', views.file_changed, name='CDAT-analysis-file'),
+
+]
+
+if settings.DEBUG:
+	urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
