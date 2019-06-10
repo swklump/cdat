edb682f86751e4d9aebd49bb7beb8d1c046d9565
Klump, Samuel W <samuel.klump@hdrinc.com>
2019-06-10 11:36:09 -0500

    Upload New File

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='HDR_CAT',
+    version='0.1dev',
+    packages=['CAT',],
+    url='http://HDRCAT.com',
+    author='RoadSafetyEIT',
+    author_email='samuel.klump@hdrinc.com',
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.txt').read(),
+)
