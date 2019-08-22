2fd0a2b8fbbbc9e65ba60c091d1d52a5909cde3b
Sam Klump <samuel.klump@hdrinc.com>
2019-08-21 17:34:39 -0700

    removed 10

--- CDAT/settings.py
+++ CDAT/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'CDAT.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'CDAT','templates')]10,
+        'DIRS': [os.path.join(BASE_DIR,'CDAT','templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
