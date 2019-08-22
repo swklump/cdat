e49af3ac73b90c16ed30f6f6fb0877a6ad0b1b19
Sam Klump <samuel.klump@hdrinc.com>
2019-08-21 17:32:22 -0700

    corrected templates path

--- CDAT/settings.py
+++ CDAT/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'CDAT.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [os.path.join(BASE_DIR,'CDAT','templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
