67066229438b4b69f075d6d3730659b077e9f58c
Sam Klump <samuel.klump@hdrinc.com>
2019-09-12 13:09:29 -0700

    Created new dist. folder/. Version 1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='HDR_CDAT',
-    version='1.0dev',
+    version='1.1dev',
     packages=['CDAT','CDAT.collision_diagrams','CDAT.collision_diagrams.modules',],
     package_data={
     'CDAT':['static/*.*','templates/*.*']},
